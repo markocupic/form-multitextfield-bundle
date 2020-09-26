84914d9dce1dde0dbfaf90c2457c9050a137ae1b
Marko Cupic <m.cupic@gmx.ch>
2020-09-26 14:25:21 +0200

    dev

---
+++ src/Resources/public/js/script.js
@@ -0,0 +1,40 @@
+(function ($) {
+    $(document).ready(function () {
+        "use strict";
+
+        /**
+         * Remove row
+         */
+        $('.form-multi-text-delete-btn').map(function(){
+            $(this).click(function(e){
+                e.preventDefault();
+                $(this).closest("tr").remove();
+            });
+        });
+
+        /**
+         * Add row
+         */
+        $('.form-multi-text-add-row-btn').map(function(){
+            $(this).click(function(e){
+                e.preventDefault();
+                let newRow = $(this).closest("tr").clone(true);
+
+                $(newRow)
+                .find(".form-multi-text-add-row-btn")
+                .text($(this).data('lang-delete-row'))
+                .removeClass("form-multi-text-add-row-btn")
+                .addClass("form-multi-text-delete-btn")
+                .closest('tr')
+                .appendTo($(this).closest("tbody"))
+                .find(".form-multi-text-delete-btn")
+                .off("click")
+                .click(function(e){
+                    e.preventDefault();
+                    remove(this);
+                });
+            });
+        });
+
+    })
+})(jQuery);
